86f8e3ea7991815882cc62f5b2e5938464732879
Chandra sekhar <63995316+vchandu111@users.noreply.github.com>
2024-10-26 13:10:27 +0530

    Class 12

---
+++ script.js
@@ -0,0 +1,10 @@
+import * as ops from "./utility.mjs";
+import divide from "./utility.mjs";
+
+console.log(ops.multiply(1, 2, 3));
+console.log(ops.addition(1, 2));
+console.log(ops.API_KEY);
+console.log(divide(4, 2));
+// import, require => node js, express js
+
+// React => import
---
+++ utility.mjs
@@ -0,0 +1,17 @@
+function multiply(a, b, c) {
+  return a * b * c;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+export const API_KEY = "abc";
+
+export default function divide(a, b) {
+  return a / b;
+}
+
+export { multiply, add as addition };
+
+// passwords, addresses, apiKeys, authentications, authorization tokens
